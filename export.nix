a5a04af1f17cbb034399853bdf427f6ca53eb2e6
Eelco Dolstra <eelco.dolstra@logicblox.com>
2008-07-24 14:35:29 +0000

    * Fix.

--- export.nix
+++ export.nix
@@ -1,8 +1,13 @@
 { stdenv, distribution }:
 
+let
+
+    distributionExport = map (entry: { component = entry.component.pkg.outPath; target = entry.target.targetEPR; } ) distribution;
+
+in
+    
 stdenv.mkDerivation rec {
     name = "distribution-export";
-    distributionExport = map (entry: { component = entry.component.pkg.outPath; target = entry.target.targetEPR; } ) distribution;
     outputXML = builtins.toXML distributionExport;
     builder = ./builder.sh;
 }
