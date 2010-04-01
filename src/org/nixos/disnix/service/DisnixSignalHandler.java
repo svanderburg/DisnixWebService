e8807c639ebb0a730b41d45778f949bdd8140dd4
Sander van der Burg <svanderburg@gmail.com>
2010-04-01 11:45:36 +0000

    - Changed license from LGPL to MIT - Updated some documentation

--- src/org/nixos/disnix/service/DisnixSignalHandler.java
+++ src/org/nixos/disnix/service/DisnixSignalHandler.java
@@ -1,77 +1,90 @@
-/*
- * DisnixService - A SOAP layer for Disnix
- * Copyright (C) 2008  Sander van der Burg
+/* 
+ * Copyright (c) 2008-2010 Sander van der Burg
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package org.nixos.disnix.service;
 import java.util.*;
 import org.freedesktop.dbus.*;
 import org.nixos.disnix.Disnix;
 
+/**
+ * Waits and reacts on D-Bus signals received from the Disnix core service
+ */
 public class DisnixSignalHandler implements DBusSigHandler
 {
+	/** Hashtable mapping job IDs on threads */
 	private Hashtable<Integer, DisnixThread> pids;
 	
+	/**
+	 * Creates a new signal handler instance
+	 */
 	public DisnixSignalHandler()
 	{
 		pids = new Hashtable<Integer, DisnixThread>();
 	}
 	
+	/**
+	 * Adds a new job on which the signal handler can react
+	 * 
+	 * @param pid Job ID received by the core service
+	 * @param thread Thread in which the job is executed
+	 */
 	public void addPid(int pid, DisnixThread thread)
 	{
 		pids.put(pid, thread);
 	}
 	
+	/**
+	 * Handles an incoming D-Bus signal
+	 * 
+	 * @param s Representation of the received D-Bus signal
+	 */
 	public void handle(DBusSignal s)
 	{	
+		DisnixThread thread = null;
+		
+		/* Only handle signals that we expect */
 		if(s instanceof Disnix.finish)
 		{
 			System.out.println("Caught finish signal!");
 			int pid = ((Disnix.finish)s).pid;			
-			DisnixThread thread = pids.remove(pid);
-			
-			if(thread != null)
-			{
-				thread.setSource(s);
-				thread.resume();
-			}
+			thread = pids.remove(pid);
 		}
 		else if(s instanceof Disnix.success)
 		{
 			System.out.println("Caught success signal!");
 			int pid = ((Disnix.success)s).pid;			
-			DisnixThread thread = pids.remove(pid);
-			
-			if(thread != null)
-			{
-				thread.setSource(s);
-				thread.resume();
-			}
+			thread = pids.remove(pid);
 		}
 		else if(s instanceof Disnix.failure)
 		{
 			System.out.println("Caught failure signal!");
 			int pid = ((Disnix.failure)s).pid;			
-			DisnixThread thread = pids.remove(pid);
-			
-			if(thread != null)
-			{
-				thread.setSource(s);
-				thread.resume();
-			}
+			thread = pids.remove(pid);
+		}
+		
+		/* Resume the thread attached to the received PID */
+		if(thread != null)
+		{
+			thread.setSource(s);
+			thread.resume();
 		}
 	}
 }
--- src/org/nixos/disnix/service/DisnixThread.java
+++ src/org/nixos/disnix/service/DisnixThread.java
@@ -1,34 +1,54 @@
-/*
- * DisnixService - A SOAP layer for Disnix
- * Copyright (C) 2008  Sander van der Burg
+/* 
+ * Copyright (c) 2008-2010 Sander van der Burg
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package org.nixos.disnix.service;
 import org.freedesktop.dbus.*;
 
+/**
+ * A thread in which a job is started on the core Disnix service and
+ * will wait until the job is finished.
+ */
 public abstract class DisnixThread implements Runnable
 {
+	/** Origin of the D-Bus signal that notifies this thread */
 	protected DBusSignal source;
+	
+	/** Indicates whether this thread is suspended */
 	private boolean suspended = false;
 	
+	/**
+	 * Sets the origin of the D-Bus signal that notifies this thread
+	 * 
+	 * @param source D-Bus signal notifying this thread
+	 */
 	public void setSource(DBusSignal source)
 	{
 		this.source = source;
 	}
 	
+	/**
+	 * Returns the origin of the D-Bus signal that notifies this thread
+	 * 
+	 * @return D-Bus signal notifying this thread
+	 */
 	public DBusSignal getSource()
 	{
 		return source;
@@ -48,11 +68,19 @@ public abstract class DisnixThread implements Runnable
 		}
 	}
 	
+	/**
+	 * Suspends this thread until it receives a notification.
+	 * 
+	 * @throws InterruptedException If this thread is interrupted
+	 */
 	public synchronized void waitForNotificationToResume() throws InterruptedException
 	{
 		while(suspended)
 			wait();
 	}
 	
+	/**
+	 * Runs the job on the core Disnix service.
+	 */
 	public abstract void run();
 }
