7ea6e5727995455a550a7f9de3bc47a79cabe6ed
Sander van der Burg <svanderburg@gmail.com>
2021-02-25 21:31:45 +0100

    Eliminate references to deprecated stdenv.lib

--- tests/deployment/pkgs/default.nix
+++ tests/deployment/pkgs/default.nix
@@ -1,17 +1,15 @@
 {pkgs}:
 
-with pkgs;
-
 rec {
   testService1 = import ./testService1.nix {
-    inherit stdenv;
+    inherit (pkgs) stdenv;
   };
-    
+
   testService2 = import ./testService2.nix {
-    inherit stdenv;
+    inherit (pkgs) stdenv lib;
   };
-  
+
   testService3 = import ./testService3.nix {
-    inherit stdenv;
+    inherit (pkgs) stdenv lib;
   };
 }
